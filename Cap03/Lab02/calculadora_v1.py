c2f1e36e84ec661dcac3dc8cf055b841b532ade3
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-08-14 13:19:57 -0300

    add python script to a functional calculator

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -6,4 +6,39 @@
 
 print("\n******************* Python Calculator *******************")
 
+def calculatorFunc():
+    
+    print('Operações disponíveis: \n1- soma \n2- subtração \n3- multiplĩcação \n4- divisão')
+
+    inpt = int(input('Selecione o número da operação que deseja realizar: 1/2/3/4 => '))
+    num1 = int(input('Digite o primeiro número: '))
+    num2 = int(input('Digite o segundo número: '))
+
+    def calc(x, y): 
+        if (inpt == 1):
+            result = x + y
+            print('%s + %s = %s' %(x, y, result))
+        elif (inpt == 2):
+            result = x - y
+            print('%s - %s = %s' %(x, y, result))
+        elif (inpt == 3):
+            result = x * y
+            print('%s x %s = %s' %(x, y, result))
+        elif (inpt == 4):
+            result = x / y
+            print('%s / %s = %s' %(x, y, result))
+        elif (input > 4):
+            print('número de operação inválido. Try again')
+
+    calc(num1, num2)
+    
+    again = input('Deseja fazer outra operação? y/n: ')
+    if again == 'y':
+        calculatorFunc()
+    elif again == 'n':
+        exit()
+        
+        
+calculatorFunc()
+
 
